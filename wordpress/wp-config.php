5eb90ff1b7800084fc5ce218bf69b963a10b03c7
Krystian Kościelniak <krystiankoscielniak@gmail.com>
2015-10-06 21:23:30 +0200

    environment change

--- wordpress/wp-config.php
+++ wordpress/wp-config.php
@@ -80,6 +80,8 @@ define('WPLANG', '');
  */
 define('WP_DEBUG', true);
 
+define('WP_ENV', 'development');
+
 /* That's all, stop editing! Happy blogging. */
 
 /** Absolute path to the WordPress directory. */
