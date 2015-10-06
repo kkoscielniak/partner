d34518ececa2a30d931c28b3b0aaea5543638d03
Krystian Kościelniak <krystiankoscielniak@gmail.com>
2015-10-06 22:25:11 +0200

    Parents section

--- wordpress/wp-content/themes/custom-name/templates/content-page.php
+++ wordpress/wp-content/themes/custom-name/templates/content-page.php
@@ -7,4 +7,5 @@
 <section class="section--text">
     <h2>Rodzice</h2>
     <p>Pomagamy rozwiązywać problemy wychowawcze i skutecznie radzić sobie z trudnymi sytuacjami. Wspieramy w tym jak być dobrym i skutecznym rodzicem, i jak stwarzać dziecku i nastolatkowi warunki do pełnego, wartościowego rozwoju. </p>
+    <!-- <a href="#">Czytaj więcej</a> -->
 </section>
