800e9a097da1ac19d519a4600485ea602f0a112a
Krystian Kościelniak <krystiankoscielniak@gmail.com>
2015-10-06 22:40:42 +0200

    adults section

--- wordpress/wp-content/themes/custom-name/templates/content-page.php
+++ wordpress/wp-content/themes/custom-name/templates/content-page.php
@@ -6,10 +6,22 @@
 
 <section class="section--text">
     <h2>Rodzice</h2>
-    <p>Pomagamy rozwiązywać problemy wychowawcze i skutecznie radzić sobie z trudnymi sytuacjami. Wspieramy w tym jak być dobrym i skutecznym rodzicem, i jak stwarzać dziecku i nastolatkowi warunki do pełnego, wartościowego rozwoju. </p>
+    <p>Pomagamy rozwiązywać problemy wychowawcze i skutecznie radzić sobie z trudnymi sytuacjami. Wspieramy w tym jak być dobrym i skutecznym rodzicem, i jak stwarzać dziecku i nastolatkowi warunki do pełnego, wartościowego rozwoju.</p>
     <!-- <a href="#">Czytaj więcej</a> -->
 </section>
 
 <section class="section--image">
     <img src="<?php echo get_template_directory_uri(); ?>/assets/img/girl-photo--mobile.png">
+</section>
+
+<section class="section--text">
+    <h2>Dzieci<br/>i młodzież</h2>
+    <p>Pomagamy rozpoznawać źródła trudności oraz potrzeby dzieci i młodzieży, kształtujemy nastawienie na rozwój i pozytywną zmianę. Rozpoznajemy trudności szkolne (np. dysleksję) oraz badamy dojrzałość szkolną. Wystawiamy opinie uznawane przez szkoły.</p>
+    <!-- <a href="#">Czytaj więcej</a> -->
+</section>
+
+<section class="section--text section--bg" style="background-image: url('<?php echo get_template_directory_uri(); ?>/assets/img/horizon.png');">
+    <h2>Dorośli</h2>
+    <p>Wspieramy dorosłych w odkrywaniu siebie oraz czerpaniu satysfakcji i przyjemności z życia. Pomagamy zmagać się z trudnościami oraz rozwijać troskliwą, opartą na miłości i wzajemnym szacunku relację z innymi. </p>
+    <!-- <a href="#">Czytaj więcej</a> -->
 </section>
