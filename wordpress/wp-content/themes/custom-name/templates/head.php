bedaaab1d04b2bdec65e620ae6a1941e3867275d
Krystian Kościelniak <krystiankoscielniak@gmail.com>
2015-11-16 12:26:48 +0100

    Contact page for desktop

--- wordpress/wp-content/themes/custom-name/templates/head.php
+++ wordpress/wp-content/themes/custom-name/templates/head.php
@@ -9,4 +9,7 @@
   <?php wp_head(); ?>
 
   <link rel="alternate" type="application/rss+xml" title="<?php echo get_bloginfo('name'); ?> Feed" href="<?php echo esc_url(get_feed_link()); ?>">
+  <?php if (is_page('contact')) { ?>
+    <script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=AIzaSyD7qqHn5eJFjWO1kR0zNWI4OHFGVCX5NCM"></script>
+  <?php } ?>
 </head>
--- wordpress/wp-content/themes/custom-name/templates/page-contact.php
+++ wordpress/wp-content/themes/custom-name/templates/page-contact.php
@@ -6,9 +6,27 @@
             <div class="grid__item palm-hide desk--one-twelfth"></div><!--
             --><div class="grid__item desk--one-quarter">
                 <p class="contact__column-headline">Kraków</p>
+                <p class="contact__address">
+                    ul. Zamoyskiego 53 lok. 4<br/>
+                    <a href="tel:+48733222600">tel: 733 222 600</a>
+                </p>
+                <p class="contact__office-hours">
+                    pon-pt:<br/>
+                    08.00-22.00<br/>
+                    SB:<br/>
+                    08.00-16.00<br/>
+                </p>
             </div><!--
             --><div class="grid__item desk--one-quarter">
                 <p class="contact__column-headline">Gliwice</p>
+                <p class="contact__address">
+                    ul. Wieczorka 2<br/>
+                    <a href="tel:+48322312690">tel: 32 231 26 90 wew. 25</a>
+                </p>
+                <p class="contact__office-hours">
+                    pon-pt:<br/>
+                    08.00-22.00<br/>
+                </p>
             </div><!--
             --><div class="grid__item palm-hide desk--one-twelfth"></div><!--
             --><div class="grid__item desk--one-third">
@@ -19,4 +37,7 @@
             </div>
         </div>
     </div>
+</section>
+<section class="map-container">
+    <div id="map-canvas"></div>
 </section>
