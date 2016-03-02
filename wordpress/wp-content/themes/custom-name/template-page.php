7ce333f273a9dc306c2792a78a5b2ddc9d509549
Krystian Kościelniak <krystiankoscielniak@gmail.com>
2016-03-02 19:24:04 +0100

    cleaning the theme and custom page template

---
+++ wordpress/wp-content/themes/custom-name/template-page.php
@@ -0,0 +1,6 @@
+<?php
+/*
+Template Name: Page Template
+*/
+?>
+<?php get_template_part('templates/template', 'page'); ?>
---
+++ wordpress/wp-content/themes/custom-name/templates/template-page.php
@@ -0,0 +1,5 @@
+<section class="banner" style="background-image: url(<?php the_field('title_background'); ?>)">
+    <div class="banner__main-content">
+        <h1 class="banner__header"><?php the_title(); ?></h1>
+    </div>
+</section>
